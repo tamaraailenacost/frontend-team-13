a8b46645e69f2d436d7c429901a2e4102f11009e
GiulianoCharra <65138966+GiulianoCharra@users.noreply.github.com>
2023-11-30 04:03:01 -0300

    arreglando variables de entorno

--- src/backend/config.py
+++ src/backend/config.py
@@ -6,9 +6,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Get the environment variables
-DB_HOST = os.getenv("localhost")
-DB_NAME = os.getenv("gym")
-DB_USER = os.getenv("root")
-DB_PASS = os.getenv("1234")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+# DB_URI = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 SECRET_KEY = os.getenv("SECRET_KEY")
--- src/backend/database.py
+++ src/backend/database.py
@@ -1,14 +1,17 @@
 import mysql.connector
 
+from src.backend import config
+
 
 class DBConnector:
     @staticmethod
     def get_connection():
         # Configura la conexión a tu base de datos MySQL
         db_config = {
-            'host': 'localhost',
-            'user': 'tu_usuario',
-            'password': 'tu_contraseña',
-            'database': 'tu_base_de_datos'
+            "host": config.DB_HOST,
+            "user": config.DB_USER,
+            "password": config.DB_PASSWORD,
+            "database": config.DB_NAME,
+            "port": config.DB_PORT,
         }
         return mysql.connector.connect(**db_config)
