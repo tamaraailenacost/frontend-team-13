104758eab354f602d8b8bed1fbb059c2e38584a1
tamaraailenacost <tamara.acosta@debricked.com>
2023-12-01 15:24:38 +0100

    mostrar clases

--- src/backend/views/calcularFecha.py
+++ src/backend/views/calcularFecha.py
@@ -1,16 +1,28 @@
 from flask import render_template, request
 from datetime import datetime
+from config import Cursor
 
-def show_class_per_date(date):
-    # Obtén el parámetro 'fecha' de la URL
-    date = request.args.get('date')
-
+# metodo que recibe una fecha y retorna las clases disponibles para ese dia de la semana
+def obtener_id_dia_semana(nombre_dia):
     try:
-        fecha = datetime.strptime(date, '%Y-%m-%d')
-        dia_semana = fecha.strftime('%A')
-        # Renderiza la plantilla con el resultado
-        print(dia_semana)
+        sql = "SELECT dia_semana_id FROM dia_semana WHERE nombre LIKE %s"
+        Cursor.execute(sql, (nombre_dia,))
+        id_dia_semana = Cursor.fetchone()
+        return id_dia_semana[0] if id_dia_semana else None
+    except Exception as e:
+        print(e)
+        return None
 
-    except ValueError:
-        # Si hay un error al analizar la fecha, muestra un mensaje de error
-        return "Formato de fecha incorrecto. Utiliza el formato YYYY-MM-DD."
+
+def obtener_clases_para_dia(id_dia_semana):
+    try:
+        if id_dia_semana:
+            sql = "SELECT clase.nombre, horario.hora_inicio FROM clase JOIN horario ON horario.horario_id = clase.horario_id WHERE horario.dia_semana_id = %s"
+            Cursor.execute(sql, (id_dia_semana,))
+            clases = Cursor.fetchall()
+            return clases
+        else:
+            return None
+    except Exception as e:
+        print(e)
+        return None
