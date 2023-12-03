4d4abf2b11c1fa1cbb92852a373e6e8270315e0f
GiulianoCharra <65138966+GiulianoCharra@users.noreply.github.com>
2023-12-03 20:54:09 -0300

    arreglado problema cuando la pass tiene @

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+from urllib.parse import quote
 
 from dotenv import load_dotenv
 
@@ -6,6 +7,16 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Get the environment variables
-SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
+MYSQL_DATABASE_HOST = os.getenv("MYSQL_DATABASE_HOST")
+MYSQL_DATABASE_PORT = os.getenv("MYSQL_DATABASE_PORT")
+MYSQL_DATABASE_USER = os.getenv("MYSQL_DATABASE_USER")
+MYSQL_DATABASE_PASSWORD = os.getenv("MYSQL_DATABASE_PASSWORD")
+MYSQL_DATABASE_DB = os.getenv("MYSQL_DATABASE_DB")
+
+encoded_password = quote(MYSQL_DATABASE_PASSWORD)
+
+# Construct SQLALCHEMY_DATABASE_URI
+SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{MYSQL_DATABASE_USER}:{encoded_password}@{MYSQL_DATABASE_HOST}:{MYSQL_DATABASE_PORT}/{MYSQL_DATABASE_DB}"
+
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = os.getenv("SECRET_KEY")
